9199969f0d91f482c81becf0cb758075524d46b8
AbdullahMufti <AbdullahMufti@users.noreply.github.com>
2020-12-06 00:38:13 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactyyyyyyyyyyyyyyyy
+          Learn Reactyyyyyyyyyyyyyyy
         </a>
       </header>
     </div>

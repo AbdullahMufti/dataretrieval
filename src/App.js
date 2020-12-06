c9774108f8f87016c94c54d166c16c23529e9dee
AbdullahMufti <AbdullahMufti@users.noreply.github.com>
2020-12-06 15:35:55 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn ڑعاچت
+          Learn ڑع ا چ ت 
         </a>
       </header>
     </div>

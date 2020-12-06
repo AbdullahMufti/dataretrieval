49ee2d5b0ae8d09a5f87e68745c18469039d064e
AbdullahMufti <AbdullahMufti@users.noreply.github.com>
2020-12-06 11:08:52 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactyyyyyyyyyyyyyyy
+          Learn Reactyyyyyyyyyyyyyyyaaaa
         </a>
       </header>
     </div>
